5b3ca4c4a4228d3dc919baf87f185a8e0baaaad4
Brandon <braondon@bharvest.io>
2021-02-22 16:26:16 +0900

    update genesis tests

--- x/liquidity/simulation/genesis_test.go
+++ x/liquidity/simulation/genesis_test.go
@@ -44,12 +44,39 @@ func TestRandomizedGenState(t *testing.T) {
 	dec4, _ := sdk.NewDecFromStr("0.378210000000000000")
 	dec5, _ := sdk.NewDecFromStr("0.438000000000000000")
 	dec6, _ := sdk.NewDecFromStr("0.448590000000000000")
-	dec7 := uint32(3)
 
 	require.Equal(t, dec1, liquidityGenesis.Params.MinInitDepositToPool)
 	require.Equal(t, dec2, liquidityGenesis.Params.InitPoolCoinMintAmount)
 	require.Equal(t, dec4, liquidityGenesis.Params.SwapFeeRate)
 	require.Equal(t, dec5, liquidityGenesis.Params.WithdrawFeeRate)
 	require.Equal(t, dec6, liquidityGenesis.Params.MaxOrderAmountRatio)
-	require.Equal(t, dec7, liquidityGenesis.Params.UnitBatchSize)
+	require.Equal(t, uint32(3), liquidityGenesis.Params.UnitBatchSize)
+}
+
+// TestRandomizedGenState tests abnormal scenarios of applying RandomizedGenState.
+func TestRandomizedGenState1(t *testing.T) {
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+	cdc := codec.NewProtoCodec(interfaceRegistry)
+
+	s := rand.NewSource(1)
+	r := rand.New(s)
+
+	// all these tests will panic
+	tests := []struct {
+		simState module.SimulationState
+		panicMsg string
+	}{
+		{ // panic => reason: incomplete initialization of the simState
+			module.SimulationState{}, "invalid memory address or nil pointer dereference"},
+		{ // panic => reason: incomplete initialization of the simState
+			module.SimulationState{
+				AppParams: make(simtypes.AppParams),
+				Cdc:       cdc,
+				Rand:      r,
+			}, "assignment to entry in nil map"},
+	}
+
+	for _, tt := range tests {
+		require.Panicsf(t, func() { simulation.RandomizedGenState(&tt.simState) }, tt.panicMsg)
+	}
 }
