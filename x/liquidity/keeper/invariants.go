4e8713cd7bc59a87fd5df994e232cfaf5178aa4f
defihyung <defihyung@gmail.com>
2021-03-16 16:39:52 +0900

    feat: wrapping up invariant checks (work in progress)

--- x/liquidity/keeper/invariants.go
+++ x/liquidity/keeper/invariants.go
@@ -63,21 +63,21 @@ var (
 
 // MintingPoolCoinsInvariant checks the correct ratio of minting amount of pool coins.
 func MintingPoolCoinsInvariant(poolCoinTotalSupply, mintPoolCoin, depositCoinA, depositCoinB, lastReserveCoinA, lastReserveCoinB sdk.Dec) {
-	// poolCoinRatio := mintPoolCoin.Quo(poolCoinTotalSupply)
-	// depositCoinARatio := depositCoinA.Quo(lastReserveCoinA)
-	// depositCoinBRatio := depositCoinB.Quo(lastReserveCoinB)
+	poolCoinRatio := mintPoolCoin.Quo(poolCoinTotalSupply)
+	depositCoinARatio := depositCoinA.Quo(lastReserveCoinA)
+	depositCoinBRatio := depositCoinB.Quo(lastReserveCoinB)
 
-	// // TODO: handle case when someone sends coins to escrow module account
+	// TODO: handle case when someone sends coins to escrow module account
 
-	// // TODO: double check if this is intended result
-	// // there may be decimal error differences which should be smaller than 1
-	// poolCoinRatio = poolCoinRatio.Add(sdk.NewDec(1.0))
+	// TODO: double check if this is intended result
+	// there may be decimal error differences which should be smaller than 1
+	poolCoinRatio = poolCoinRatio.Add(sdk.NewDec(1.0))
 
-	// // NewPoolCoinAmount / LastPoolCoinSupply = DepositCoinA / LastReserveCoinA
-	// // NewPoolCoinAmount / LastPoolCoinSupply = DepositCoinB / LastReserveCoinB
-	// if !poolCoinRatio.GTE(depositCoinARatio) || !poolCoinRatio.GTE(depositCoinBRatio) {
-	// 	panic("invariant check fails due to incorrect ratio of pool coins")
-	// }
+	// NewPoolCoinAmount / LastPoolCoinSupply = DepositCoinA / LastReserveCoinA
+	// NewPoolCoinAmount / LastPoolCoinSupply = DepositCoinB / LastReserveCoinB
+	if !poolCoinRatio.GTE(depositCoinARatio) || !poolCoinRatio.GTE(depositCoinBRatio) {
+		panic("invariant check fails due to incorrect ratio of pool coins")
+	}
 }
 
 // DepositReserveCoinsInvariant checks the after deposit amounts.
@@ -100,7 +100,7 @@ func DepositReserveCoinsInvariant(depositCoinA, depositCoinB, lastReserveCoinA, 
 func DepositRatioInvariant(depositCoinA, depositCoinB, lastReserveCoinRatio sdk.Dec) {
 	// depositCoinRatio := depositCoinA.Quo(depositCoinB)
 
-	// DepositCoinA / DepositCoinB = LastReserveCoinA / LastReserveCoinB
+	// // DepositCoinA / DepositCoinB = LastReserveCoinA / LastReserveCoinB
 	// if !depositCoinRatio.Equal(lastReserveCoinRatio) {
 	// 	panic("invariant check fails due to incorrect deposit ratio")
 	// }
@@ -116,38 +116,38 @@ func ImmutablePoolPriceAfterDepositInvariant(lastReserveCoinRatio, afterReserveC
 
 // BurningPoolCoinsInvariant checks the correct burning amount of pool coins.
 func BurningPoolCoinsInvariant(burnedPoolCoin, withdrawCoinA, withdrawCoinB, reserveCoinA, reserveCoinB, lastPoolCoinSupply, withdrawProportion sdk.Dec) {
-	// burningPoolCoinRatio := burnedPoolCoin.Quo(lastPoolCoinSupply)
-	// withdrawCoinARatio := withdrawCoinA.Add(withdrawProportion).Quo(reserveCoinA)
-	// withdrawCoinBRatio := withdrawCoinB.Add(withdrawProportion).Quo(reserveCoinB)
+	burningPoolCoinRatio := burnedPoolCoin.Quo(lastPoolCoinSupply)
+	withdrawCoinARatio := withdrawCoinA.Add(withdrawProportion).Quo(reserveCoinA)
+	withdrawCoinBRatio := withdrawCoinB.Add(withdrawProportion).Quo(reserveCoinB)
 
-	// // TODO: double check if this is intended result
-	// // there may be decimal error differences which should be smaller than 1
-	// burningPoolCoinRatio = burningPoolCoinRatio.Add(sdk.NewDec(1))
+	// TODO: double check if this is intended result
+	// there may be decimal error differences which should be smaller than 1
+	burningPoolCoinRatio = burningPoolCoinRatio.Add(sdk.NewDec(1))
 
-	// // BurnedPoolCoinAmount / LastPoolCoinSupply = (WithdrawCoinA+WithdrawFeeCoinA) / LastReserveCoinA
-	// // BurnedPoolCoinAmount / LastPoolCoinSupply = (WithdrawCoinB+WithdrawFeeCoinB) / LastReserveCoinB
-	// if !burningPoolCoinRatio.GTE(withdrawCoinARatio) || !burningPoolCoinRatio.GTE(withdrawCoinBRatio) {
-	// 	panic("invariant check fails due to incorrect ratio of burning pool coins")
-	// }
+	// BurnedPoolCoinAmount / LastPoolCoinSupply = (WithdrawCoinA+WithdrawFeeCoinA) / LastReserveCoinA
+	// BurnedPoolCoinAmount / LastPoolCoinSupply = (WithdrawCoinB+WithdrawFeeCoinB) / LastReserveCoinB
+	if !burningPoolCoinRatio.GTE(withdrawCoinARatio) || !burningPoolCoinRatio.GTE(withdrawCoinBRatio) {
+		panic("invariant check fails due to incorrect ratio of burning pool coins")
+	}
 }
 
 // WithdrawReserveCoinsInvariant checks the after withdraw amounts.
 func WithdrawReserveCoinsInvariant(withdrawCoinA, withdrawCoinB, reserveCoinA, reserveCoinB,
 	afterReserveCoinA, afterReserveCoinB, afterPoolCoinTotalSupply, lastPoolCoinSupply, burnedPoolCoin sdk.Dec) {
-	// // AfterWithdrawReserveCoinA = LastReserveCoinA - WithdrawCoinA
-	// if !afterReserveCoinA.Equal(reserveCoinA.Sub(withdrawCoinA)) {
-	// 	panic("invariant check fails due to incorrect withdraw coin A amount")
-	// }
+	// AfterWithdrawReserveCoinA = LastReserveCoinA - WithdrawCoinA
+	if !afterReserveCoinA.Equal(reserveCoinA.Sub(withdrawCoinA)) {
+		panic("invariant check fails due to incorrect withdraw coin A amount")
+	}
 
-	// // AfterWithdrawReserveCoinB = LastReserveCoinB - WithdrawCoinB
-	// if !afterReserveCoinB.Equal(reserveCoinB.Sub(withdrawCoinB)) {
-	// 	panic("invariant check fails due to incorrect withdraw coin B amount")
-	// }
+	// AfterWithdrawReserveCoinB = LastReserveCoinB - WithdrawCoinB
+	if !afterReserveCoinB.Equal(reserveCoinB.Sub(withdrawCoinB)) {
+		panic("invariant check fails due to incorrect withdraw coin B amount")
+	}
 
-	// // AfterWithdrawPoolCoinSupply = LastPoolCoinSupply - BurnedPoolCoinAmount
-	// if !afterPoolCoinTotalSupply.Equal(lastPoolCoinSupply.Sub(burnedPoolCoin)) {
-	// 	panic("invariant check fails due to incorrect total supply")
-	// }
+	// AfterWithdrawPoolCoinSupply = LastPoolCoinSupply - BurnedPoolCoinAmount
+	if !afterPoolCoinTotalSupply.Equal(lastPoolCoinSupply.Sub(burnedPoolCoin)) {
+		panic("invariant check fails due to incorrect total supply")
+	}
 }
 
 // WithdrawRatioInvariant checks the correct ratio of withdraw coin amounts.
@@ -175,157 +175,157 @@ func ImmutablePoolPriceAfterWithdrawInvariant(reserveCoinA, reserveCoinB, afterR
 // SwapPriceInvariants checks the calculated swap price is increased, decreased, or equal from the last pool price.
 func SwapPriceInvariants(XtoY, YtoX []*types.SwapMsgState, matchResultXtoY, matchResultYtoX []types.MatchResult,
 	fractionalCntX, fractionalCntY int, poolXdelta, poolYdelta, poolXdelta2, poolYdelta2, decimalErrorX, decimalErrorY sdk.Dec, result types.BatchResult) {
-	// beforeXtoYLen := len(XtoY)
-	// beforeYtoXLen := len(YtoX)
-	// if beforeXtoYLen-len(matchResultXtoY)+fractionalCntX != types.CountNotMatchedMsgs(XtoY)+types.CountFractionalMatchedMsgs(XtoY) {
-	// 	panic(beforeXtoYLen)
-	// }
-	// if beforeYtoXLen-len(matchResultYtoX)+fractionalCntY != types.CountNotMatchedMsgs(YtoX)+types.CountFractionalMatchedMsgs(YtoX) {
-	// 	panic(beforeYtoXLen)
-	// }
+	beforeXtoYLen := len(XtoY)
+	beforeYtoXLen := len(YtoX)
+	if beforeXtoYLen-len(matchResultXtoY)+fractionalCntX != types.CountNotMatchedMsgs(XtoY)+types.CountFractionalMatchedMsgs(XtoY) {
+		panic(beforeXtoYLen)
+	}
+	if beforeYtoXLen-len(matchResultYtoX)+fractionalCntY != types.CountNotMatchedMsgs(YtoX)+types.CountFractionalMatchedMsgs(YtoX) {
+		panic(beforeYtoXLen)
+	}
 
-	// totalAmtX := sdk.ZeroDec()
-	// totalAmtY := sdk.ZeroDec()
+	totalAmtX := sdk.ZeroDec()
+	totalAmtY := sdk.ZeroDec()
 
-	// for _, mr := range matchResultXtoY {
-	// 	totalAmtX = totalAmtX.Sub(mr.TransactedCoinAmt)
-	// 	totalAmtY = totalAmtY.Add(mr.ExchangedDemandCoinAmt)
-	// }
+	for _, mr := range matchResultXtoY {
+		totalAmtX = totalAmtX.Sub(mr.TransactedCoinAmt)
+		totalAmtY = totalAmtY.Add(mr.ExchangedDemandCoinAmt)
+	}
 
-	// invariantCheckX := totalAmtX
-	// invariantCheckY := totalAmtY
+	invariantCheckX := totalAmtX
+	invariantCheckY := totalAmtY
 
-	// totalAmtX = sdk.ZeroDec()
-	// totalAmtY = sdk.ZeroDec()
+	totalAmtX = sdk.ZeroDec()
+	totalAmtY = sdk.ZeroDec()
 
-	// for _, mr := range matchResultYtoX {
-	// 	totalAmtY = totalAmtY.Sub(mr.TransactedCoinAmt)
-	// 	totalAmtX = totalAmtX.Add(mr.ExchangedDemandCoinAmt)
-	// }
+	for _, mr := range matchResultYtoX {
+		totalAmtY = totalAmtY.Sub(mr.TransactedCoinAmt)
+		totalAmtX = totalAmtX.Add(mr.ExchangedDemandCoinAmt)
+	}
 
-	// invariantCheckX = invariantCheckX.Add(totalAmtX)
-	// invariantCheckY = invariantCheckY.Add(totalAmtY)
+	invariantCheckX = invariantCheckX.Add(totalAmtX)
+	invariantCheckY = invariantCheckY.Add(totalAmtY)
 
-	// invariantCheckX = invariantCheckX.Add(poolXdelta)
-	// invariantCheckY = invariantCheckY.Add(poolYdelta)
+	invariantCheckX = invariantCheckX.Add(poolXdelta)
+	invariantCheckY = invariantCheckY.Add(poolYdelta)
 
-	// // print the invariant check and validity with swap, match result
-	// if invariantCheckX.IsZero() && invariantCheckY.IsZero() {
-	// } else {
-	// 	panic(invariantCheckX)
-	// }
+	// print the invariant check and validity with swap, match result
+	if invariantCheckX.IsZero() && invariantCheckY.IsZero() {
+	} else {
+		panic(invariantCheckX)
+	}
 
-	// if !poolXdelta.Add(decimalErrorX).Equal(poolXdelta2) || !poolYdelta.Add(decimalErrorY).Equal(poolYdelta2) {
-	// 	panic(poolXdelta)
-	// }
+	if !poolXdelta.Add(decimalErrorX).Equal(poolXdelta2) || !poolYdelta.Add(decimalErrorY).Equal(poolYdelta2) {
+		panic(poolXdelta)
+	}
 
-	// validitySwapPrice := types.CheckSwapPrice(matchResultXtoY, matchResultYtoX, result.SwapPrice)
-	// if !validitySwapPrice {
-	// 	panic("validitySwapPrice")
-	// }
+	validitySwapPrice := types.CheckSwapPrice(matchResultXtoY, matchResultYtoX, result.SwapPrice)
+	if !validitySwapPrice {
+		panic("validitySwapPrice")
+	}
 }
 
 // OrdersWithNotExecutedStateInvariants checks all executed orders have order price which is not "executable" or not "unexecutable".
 func OrdersWithExecutedAndNotExecutedStateInvariants(matchResultXtoY, matchResultYtoX []types.MatchResult, matchResultMap map[uint64]types.MatchResult,
 	swapMsgStates []*types.SwapMsgState, XtoY, YtoX []*types.SwapMsgState, result types.BatchResult, currentPoolPrice sdk.Dec, denomX string) {
-	// if len(matchResultXtoY)+len(matchResultYtoX) != len(matchResultMap) {
-	// 	panic("invalid length of match result")
-	// }
+	if len(matchResultXtoY)+len(matchResultYtoX) != len(matchResultMap) {
+		panic("invalid length of match result")
+	}
 
-	// // compare swapMsgs state with XtoY, YtoX
-	// for k, v := range matchResultMap {
-	// 	if k != v.OrderMsgIndex {
-	// 		panic("broken map consistency")
-	// 	}
-	// }
+	// compare swapMsgs state with XtoY, YtoX
+	for k, v := range matchResultMap {
+		if k != v.OrderMsgIndex {
+			panic("broken map consistency")
+		}
+	}
 
-	// for _, sms := range swapMsgStates {
-	// 	for _, smsXtoY := range XtoY {
-	// 		if sms.MsgIndex == smsXtoY.MsgIndex {
-	// 			if *(sms) != *(smsXtoY) || sms != smsXtoY {
-	// 				panic("swap message state not matched")
-	// 			} else {
-	// 				break
-	// 			}
-	// 		}
-	// 	}
+	for _, sms := range swapMsgStates {
+		for _, smsXtoY := range XtoY {
+			if sms.MsgIndex == smsXtoY.MsgIndex {
+				if *(sms) != *(smsXtoY) || sms != smsXtoY {
+					panic("swap message state not matched")
+				} else {
+					break
+				}
+			}
+		}
 
-	// 	for _, smsYtoX := range YtoX {
-	// 		if sms.MsgIndex == smsYtoX.MsgIndex {
-	// 			if *(sms) != *(smsYtoX) || sms != smsYtoX {
-	// 				panic("swap message state not matched")
-	// 			} else {
-	// 				break
-	// 			}
-	// 		}
-	// 	}
+		for _, smsYtoX := range YtoX {
+			if sms.MsgIndex == smsYtoX.MsgIndex {
+				if *(sms) != *(smsYtoX) || sms != smsYtoX {
+					panic("swap message state not matched")
+				} else {
+					break
+				}
+			}
+		}
 
-	// 	if msgAfter, ok := matchResultMap[sms.MsgIndex]; ok {
-	// 		if sms.MsgIndex == msgAfter.BatchMsg.MsgIndex {
-	// 			if *(sms) != *(msgAfter.BatchMsg) || sms != msgAfter.BatchMsg {
-	// 				panic("msg not matched")
-	// 			} else {
-	// 				break
-	// 			}
-	// 		} else {
-	// 			panic("fail msg pointer consistency")
-	// 		}
-	// 	}
-	// }
+		if msgAfter, ok := matchResultMap[sms.MsgIndex]; ok {
+			if sms.MsgIndex == msgAfter.BatchMsg.MsgIndex {
+				if *(sms) != *(msgAfter.BatchMsg) || sms != msgAfter.BatchMsg {
+					panic("msg not matched")
+				} else {
+					break
+				}
+			} else {
+				panic("fail msg pointer consistency")
+			}
+		}
+	}
 
-	// // checks whether the calculated swapPrice is increased / decreased/ or stayed from the last pool price
-	// switch result.PriceDirection {
-	// case types.Increasing:
-	// 	if !result.SwapPrice.GTE(currentPoolPrice) {
-	// 		panic("invariant check fails due to increase of swap price")
-	// 	}
-	// case types.Decreasing:
-	// 	if !result.SwapPrice.LTE(currentPoolPrice) {
-	// 		panic("invariant check fails due to decrease of swap price")
-	// 	}
-	// case types.Staying:
-	// 	if !result.SwapPrice.Equal(currentPoolPrice) {
-	// 		panic("invariant check fails due to stay of swap price")
-	// 	}
-	// }
+	// checks whether the calculated swapPrice is increased / decreased/ or stayed from the last pool price
+	switch result.PriceDirection {
+	case types.Increasing:
+		if !result.SwapPrice.GTE(currentPoolPrice) {
+			panic("invariant check fails due to increase of swap price")
+		}
+	case types.Decreasing:
+		if !result.SwapPrice.LTE(currentPoolPrice) {
+			panic("invariant check fails due to decrease of swap price")
+		}
+	case types.Staying:
+		if !result.SwapPrice.Equal(currentPoolPrice) {
+			panic("invariant check fails due to stay of swap price")
+		}
+	}
 
-	// // invariant check, execution validity check
-	// for _, sms := range swapMsgStates {
-	// 	if _, ok := matchResultMap[sms.MsgIndex]; ok {
-	// 		// checks whether all executed orders have order price which is not "unexecutable"
-	// 		if !sms.Executed || !sms.Succeeded {
-	// 			panic("swap msg state consistency error, matched but not succeeded")
-	// 		}
+	// invariant check, execution validity check
+	for _, sms := range swapMsgStates {
+		if _, ok := matchResultMap[sms.MsgIndex]; ok {
+			// checks whether all executed orders have order price which is not "unexecutable"
+			if !sms.Executed || !sms.Succeeded {
+				panic("swap msg state consistency error, matched but not succeeded")
+			}
 
-	// 		if sms.Msg.OfferCoin.Denom == denomX {
-	// 			// buy orders having equal or higher order price than found swapPrice
-	// 			if !sms.Msg.OrderPrice.GTE(result.SwapPrice) {
-	// 				panic("execution validity failed, executed but unexecutable")
-	// 			}
-	// 		} else {
-	// 			// sell orders having equal or lower order price than found swapPrice
-	// 			if !sms.Msg.OrderPrice.LTE(result.SwapPrice) {
-	// 				panic("execution validity failed, executed but unexecutable")
-	// 			}
-	// 		}
+			if sms.Msg.OfferCoin.Denom == denomX {
+				// buy orders having equal or higher order price than found swapPrice
+				if !sms.Msg.OrderPrice.GTE(result.SwapPrice) {
+					panic("execution validity failed, executed but unexecutable")
+				}
+			} else {
+				// sell orders having equal or lower order price than found swapPrice
+				if !sms.Msg.OrderPrice.LTE(result.SwapPrice) {
+					panic("execution validity failed, executed but unexecutable")
+				}
+			}
 
-	// 	} else {
-	// 		// check whether every unexecuted orders have order price which is not "executable"
-	// 		if sms.Executed && sms.Succeeded {
-	// 			panic("sms consistency error, not matched but succeeded")
-	// 		}
+		} else {
+			// check whether every unexecuted orders have order price which is not "executable"
+			if sms.Executed && sms.Succeeded {
+				panic("sms consistency error, not matched but succeeded")
+			}
 
-	// 		if sms.Msg.OfferCoin.Denom == denomX {
-	// 			// buy orders having equal or lower order price than found swapPrice
-	// 			if !sms.Msg.OrderPrice.LTE(result.SwapPrice) {
-	// 				panic("execution validity failed, unexecuted but executable")
-	// 			}
-	// 		} else {
-	// 			// sell orders having equal or higher order price than found swapPrice
-	// 			if !sms.Msg.OrderPrice.GTE(result.SwapPrice) {
-	// 				panic("execution validity failed, unexecuted but executable")
-	// 			}
-	// 		}
-	// 	}
-	// }
+			if sms.Msg.OfferCoin.Denom == denomX {
+				// buy orders having equal or lower order price than found swapPrice
+				if !sms.Msg.OrderPrice.LTE(result.SwapPrice) {
+					panic("execution validity failed, unexecuted but executable")
+				}
+			} else {
+				// sell orders having equal or higher order price than found swapPrice
+				if !sms.Msg.OrderPrice.GTE(result.SwapPrice) {
+					panic("execution validity failed, unexecuted but executable")
+				}
+			}
+		}
+	}
 }
