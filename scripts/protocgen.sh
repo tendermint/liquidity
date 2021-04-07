2a73a69e7affba7eab9e0ea4dab04359bab1a335
Hanjun Kim <hallazzang@gmail.com>
2021-04-07 15:55:43 +0900

    make proto-gen and proto-swagger-gen to use docker
    it makes proto stub generation more deterministic

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,14 +2,25 @@
 
 set -eo pipefail
 
+protoc_gen_gocosmos() {
+  if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
+    echo -e "\tPlease run this command from somewhere inside the cosmos-sdk folder."
+    return 1
+  fi
+
+  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@latest 2>/dev/null
+}
+
+protoc_gen_gocosmos
+
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --gocosmos_out=plugins=interfacetype+grpc,\
+  buf protoc \
+    -I "proto" \
+    -I "third_party/proto" \
+    --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  --grpc-gateway_out=logtostderr=true:. \
+    --grpc-gateway_out=logtostderr=true,allow_colon_final_segments=true:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
 
 done
