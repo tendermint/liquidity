2f68a1ba5b3fc0faf088daf8e0ff39306f1be9c0
dongsam <dongsamb@gmail.com>
2021-04-07 16:53:14 +0900

    temporarily remove to create diff for godoc review

--- app/params/weights.go
+++
@@ -1,9 +0,0 @@
-package params
-
-const (
-	// liquidity module simulation operation weights for messages
-	DefaultWeightMsgCreatePool          int = 5
-	DefaultWeightMsgDepositWithinBatch  int = 10
-	DefaultWeightMsgWithdrawWithinBatch int = 50
-	DefaultWeightMsgSwapWithinBatch     int = 85
-)
